f938d7b2c2d8982ca9bbcfb85e80186c75ee79eb
Mayuri <mayurigurav1997@gmail.com>
2024-04-04 23:18:37 +0530

    testes for number transformation

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,15 @@
+import {it,expect} from 'vitest'
+
+import { transformToNumber } from './numbers'
+
+it("should transform a string number to number of type number",()=>{
+    const input = "1"
+    const result = transformToNumber(input)
+    expect(result).toBeTypeOf('number')
+})
+
+it("should yield NaN for non-transformable values",()=>{
+    const input = "mayuri"
+    const result = transformToNumber(input)
+    expect(result).toBeNaN()
+})
