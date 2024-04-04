7e3fa20949446f0ccb70ef042dbd62f2ac0ca3dd
Mayuri <mayurigurav1997@gmail.com>
2024-04-04 20:54:44 +0530

    arrange act assert used

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,8 +1,15 @@
 import {test,it,expect} from "vitest"
 import {add} from "./math"
 
-it('shoul do sum of numbers in array',()=>{
-     const result = add([1,2,3])
-     expect(result).toBe(6)
-     
+it('should do sum of numbers in array',()=>{
+    //arrange
+    const numbers=[1,2,3]
+   
+    //act
+     const result = add(numbers)
+
+     //assert
+     const expectedResult= numbers.reduce((prevVal,currVal)=> prevVal+ currVal,0)
+     expect(result).toBe(expectedResult)
+
 })
