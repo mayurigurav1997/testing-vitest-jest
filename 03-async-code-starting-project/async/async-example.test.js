6bf53a210703d0684e47c6c2ebb3e2238607b3f6
Mayuri <mayurigurav1997@gmail.com>
2024-06-25 22:58:18 +0530

    async with callback

--- 03-async-code-starting-project/async/async-example.test.js
+++ 03-async-code-starting-project/async/async-example.test.js
@@ -1,7 +1,17 @@
-import {expect, it} from "vitest"
-import {generateToken} from './async-example'
+import { expect, it } from "vitest";
+import { generateToken } from "./async-example";
 
-it('should generate a token value', ()=>{
-    const testUserEmail= 'test@test.com'
-    generateToken(testUserEmail,(err, token)=>{expect(token).toBeDefined()})
-})
+it("should generate a token value", (done) => {
+  const testUserEmail = "test@test.com";
+  generateToken(testUserEmail, (err, token) => {
+    try{
+        expect(token).toBeDefined();
+        // expect(token).toBe(2)
+        done()
+    }catch(err){
+        done(err)
+    }
+    
+   
+  });
+});
