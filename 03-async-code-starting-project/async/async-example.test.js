8be5764d2c9e79ffe882b28af7109031778b6e4f
Mayuri <mayurigurav1997@gmail.com>
2024-06-25 22:49:40 +0530

    async function testing

---
+++ 03-async-code-starting-project/async/async-example.test.js
@@ -0,0 +1,7 @@
+import {expect, it} from "vitest"
+import {generateToken} from './async-example'
+
+it('should generate a token value', ()=>{
+    const testUserEmail= 'test@test.com'
+    generateToken(testUserEmail,(err, token)=>{expect(token).toBeDefined()})
+})
