b37a6fcbfd59de2a5ef76122c0b8c8035cde5333
Mayuri <mayurigurav1997@gmail.com>
2024-06-25 23:29:59 +0530

    after before each all

--- 05-hooks-starting-code/hooks/hooks.test.js
+++ 05-hooks-starting-code/hooks/hooks.test.js
@@ -1,35 +1,43 @@
-import { it, expect } from "vitest";
+import { it, expect, beforeAll,beforeEach,afterEach,afterAll } from "vitest";
 
 import { User } from "./hooks";
 const testEmail = "test@test.com";
-// const user = new User(testEmail);
+let user
+beforeAll(()=>{
+  user = new User(testEmail);
+})
+beforeEach(()=>{
+  user = new User(testEmail);
+})
+afterEach(()=>{
+  //  user = new User(testEmail);
+})
+afterAll(()=>{
+
+})
 it("should update the email", () => {
   const newTestEmail = "test2@test.com";
-  const user = new User(testEmail);
+
   user.updateEmail(newTestEmail);
 
   expect(user.email).toBe(newTestEmail);
 });
 
 it("should have an email property", () => {
-  const user = new User(testEmail);
   expect(user).toHaveProperty("email");
 });
 
 it("should store the provided email value", () => {
-  const user = new User(testEmail);
   expect(user.email).toBe(testEmail);
 });
 
 it("should clear the email", () => {
-  const user = new User(testEmail);
   user.clearEmail();
 
   expect(user.email).toBe("");
 });
 
 it("should still have an email property after clearing the email", () => {
-  const user = new User(testEmail);
   user.clearEmail();
 
   expect(user).toHaveProperty("email");
